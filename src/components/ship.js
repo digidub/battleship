b505c2000047337a9d3cf5eddc9fb22e8cffc0d3
Sholto <madein.mania@gmail.com>
2021-07-28 10:29:24 +0100

    initial array of ships object

--- src/components/ship.js
+++ src/components/ship.js
@@ -1,1 +1,28 @@
-//factory function to produce ships. Ships are objects that include their length, where they have been hit, and whether or not they've been sunk
+// factory function to produce ships. Ships are objects
+// that include their length, where they have been hit, and whether or not they've been sunk
+
+const Ship = () => {
+  // needs to produce multiple ships - 2, 3, 3, 4, 5
+  const ships = [
+    {
+      name: 'destroyer',
+      ship: ['x', 'x'],
+    },
+    {
+      name: 'submarine',
+      ship: ['x', 'x', 'x'],
+    },
+    {
+      name: 'cruiser',
+      ship: ['x', 'x', 'x'],
+    },
+    {
+      name: 'battleship',
+      ship: ['x', 'x', 'x', 'x'],
+    },
+    {
+      name: 'carrier',
+      ship: ['x', 'x', 'x', 'x', 'x'],
+    },
+  ];
+};
