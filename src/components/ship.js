d4e1e7f34db0602eb526e8d78e4b42726d8a31ca
Sholto <madein.mania@gmail.com>
2021-07-29 11:43:03 +0100

    refactored code to smaller factory function with arguments

--- src/components/ship.js
+++ src/components/ship.js
@@ -1,38 +1,48 @@
 // factory function to produce ships. Ships are objects
 // that include their length, where they have been hit, and whether or not they've been sunk
 
-const Ship = () => {
-  // needs to produce multiple ships - 2, 3, 3, 4, 5
-  const ships = [
-    {
-      name: 'destroyer',
-      length: 2,
-      sunk: false,
-      ship: [false, false],
-    },
-    {
-      name: 'submarine',
-      length: 3,
-      sunk: false,
-      ship: [false, false, false],
-    },
-    {
-      name: 'cruiser',
-      length: 3,
-      sunk: false,
-      ship: [false, false, false],
-    },
-    {
-      name: 'battleship',
-      length: 4,
-      sunk: false,
-      ship: [false, false, false, false],
-    },
-    {
-      name: 'carrier',
-      length: 5,
-      sunk: false,
-      ship: [false, false, false, false, false],
-    },
-  ];
-};
+const Ship = (name, length, orientation = 'horizontal') => ({
+  name,
+  length,
+  orientation,
+  // sunk: false,
+  // hits: new Array(length).fill(false),
+  // hit(coordinates) {},
+});
+
+//   {
+//   ships: [
+//     {
+//       name: 'destroyer',
+//       length: 2,
+//       sunk: false,
+//       hits: [false, false],
+//     },
+//     {
+//       name: 'submarine',
+//       length: 3,
+//       sunk: false,
+//       hits: [false, false, false],
+//     },
+//     {
+//       name: 'cruiser',
+//       length: 3,
+//       sunk: false,
+//       hits: [false, false, false],
+//     },
+//     {
+//       name: 'battleship',
+//       length: 4,
+//       sunk: false,
+//       hits: [false, false, false, false],
+//     },
+//     {
+//       name: 'carrier',
+//       length: 5,
+//       sunk: false,
+//       hits: [false, false, false, false, false],
+//     }
+//   ],
+//   hit: function(coordinates) => {};
+// }};
+module.exports = Ship;
