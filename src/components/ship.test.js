906001ef2aaa98b5cd59eb6128c5ffb21b93b569
Sholto <madein.mania@gmail.com>
2021-08-24 14:56:38 +0100

    updated tests - first two using partial matchers to ensure object properties are present and correct, third test to ensure hit method works

--- src/components/ship.test.js
+++ src/components/ship.test.js
@@ -1,21 +1,36 @@
+/* eslint-disable comma-dangle */
 const Ship = require('./ship');
 
 test('produces a basic ship object', () => {
-  expect(Ship('shippy', 3)).toStrictEqual({
-    name: 'shippy',
-    length: 3,
-    orientation: 'horizontal',
-    sunk: false,
-    hits: [false, false, false],
-  });
+  expect(Ship('shippy', 3)).toStrictEqual(
+    expect.objectContaining({
+      name: 'shippy',
+      length: 3,
+      orientation: 'horizontal',
+      sunk: false,
+      hits: [false, false, false],
+    })
+  );
 });
 
 test('produces a longer ship object', () => {
-  expect(Ship('longerShip', 5)).toStrictEqual({
-    name: 'longerShip',
-    length: 5,
-    orientation: 'horizontal',
-    sunk: false,
-    hits: [false, false, false, false, false],
+  expect(Ship('longerShip', 5)).toStrictEqual(
+    expect.objectContaining({
+      name: 'longerShip',
+      length: 5,
+      orientation: 'horizontal',
+      sunk: false,
+      hits: [false, false, false, false, false],
+    })
+  );
+});
+
+describe('ship', () => {
+  describe('hit position', () => {
+    it('hits a given position in the hits array', () => {
+      const shipOne = Ship('testShip', 4);
+      shipOne.hit(3);
+      expect(shipOne.hits).toStrictEqual([false, false, false, true]);
+    });
   });
 });
