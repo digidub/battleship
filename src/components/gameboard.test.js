c6545d5db96f678797f0bcff444f7193818a967e
Sholto <madein.mania@gmail.com>
2021-08-25 10:41:23 +0100

    test for ship placement

--- src/components/gameboard.test.js
+++ src/components/gameboard.test.js
@@ -1,17 +1,27 @@
 const Gameboard = require('./gameboard');
+const Ship = require('./ship');
 
 describe('Gameboard', () => {
   describe('creates grid', () => {
     it('creates a 10x10 grid', () => {
-      const grid = Gameboard();
-      grid.buildGrid();
-      expect(grid.grid.length).toEqual(10);
-      expect(grid.grid[1].length).toEqual(10);
-      expect(grid.grid[2].length).toEqual(10);
-      expect(grid.grid[4].length).toEqual(10);
-      expect(grid.grid[6].length).toEqual(10);
-      expect(grid.grid[8].length).toEqual(10);
-      expect(grid.grid[9].length).toEqual(10);
+      const game = Gameboard();
+      game.buildGrid();
+      expect(game.grid.length).toEqual(10);
+      expect(game.grid[1].length).toEqual(10);
+      expect(game.grid[2].length).toEqual(10);
+      expect(game.grid[4].length).toEqual(10);
+      expect(game.grid[6].length).toEqual(10);
+      expect(game.grid[8].length).toEqual(10);
+      expect(game.grid[9].length).toEqual(10);
+    });
+  });
+  describe('ship placement', () => {
+    it('places a ship', () => {
+      const game = Gameboard();
+      const ship = Ship('small', 3);
+      game.buildGrid();
+      game.placeShip(ship, 0, 0);
+      expect(game.grid[0]).toEqual(['small', 'small', 'small', '03', '04', '05', '06', '07', '08', '09']);
     });
   });
 });
