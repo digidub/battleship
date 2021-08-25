0982b363538341ed8ae705b84b8bf670930245fc
Sholto <madein.mania@gmail.com>
2021-08-25 11:26:08 +0100

    refactored gameboard function to produce null array cells

--- src/components/gameboard.js
+++ src/components/gameboard.js
@@ -5,15 +5,15 @@ const Gameboard = () => {
 
   const buildGrid = () => {
     grid = new Array(10);
+    grid.fill(null, 0);
     for (let i = 0; i < grid.length; i += 1) {
       grid[i] = new Array(10);
-      for (let j = 0; j < grid[i].length; j += 1) {
-        grid[i][j] = `${i}${j}`;
-      }
+      grid[i].fill(null, 0);
     }
   };
 
   const placeShip = (ship, i, j) => {
+    if (grid[i][j] !== null) return;
     const gridCellsUsed = ship.length;
     if (ship.orientation === 'horizontal') {
       for (let x = 0; x < gridCellsUsed; x += 1) {
