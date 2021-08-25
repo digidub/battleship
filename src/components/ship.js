c9201f33d8367c31c4dd92e2bc318920ddba0fdf
Sholto <madein.mania@gmail.com>
2021-08-25 09:37:27 +0100

    method and getter for orientation

--- src/components/ship.js
+++ src/components/ship.js
@@ -1,8 +1,10 @@
 // factory function to produce ships. Ships are objects
 // that include their length, where they have been hit, and whether or not they've been sunk
 
-const Ship = (name, length, orientation = 'horizontal') => {
+const Ship = (name, length) => {
   let sunk = false;
+  let orientation = 'horizontal';
+
   const hits = Array(length).fill(false);
 
   const hit = (position) => {
@@ -13,18 +15,26 @@ const Ship = (name, length, orientation = 'horizontal') => {
     if (!hits.includes(false)) sunk = true;
   };
 
+  const switchOrientation = () => {
+    if (orientation === 'horizontal') orientation = 'vertical';
+    else orientation = 'horizontal';
+  };
+
   return {
     name,
     length,
-    orientation,
     hit,
     sink,
+    switchOrientation,
     // get values using getters
     get sunk() {
       return sunk;
     },
     get hits() {
       return hits;
+    },
+    get orientation() {
+      return orientation;
     },
   };
 };
