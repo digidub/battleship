c8cfb09351044c13f973ee74b733707d0f0591f2
Sholto <madein.mania@gmail.com>
2021-08-25 10:56:40 +0100

    test for placement of multiple horizontal ships

--- src/components/gameboard.test.js
+++ src/components/gameboard.test.js
@@ -16,12 +16,32 @@ describe('Gameboard', () => {
     });
   });
   describe('ship placement', () => {
-    it('places a ship', () => {
+    it('places a ship in the first coordinates of the first row', () => {
       const game = Gameboard();
       const ship = Ship('small', 3);
       game.buildGrid();
       game.placeShip(ship, 0, 0);
       expect(game.grid[0]).toEqual(['small', 'small', 'small', '03', '04', '05', '06', '07', '08', '09']);
+    });
+    it('places a ship in the last coordinates of the first row', () => {
+      const game = Gameboard();
+      const ship = Ship('small', 2);
+      game.buildGrid();
+      game.placeShip(ship, 0, 8);
+      expect(game.grid[0]).toEqual(['00', '01', '02', '03', '04', '05', '06', '07', 'small', 'small']);
+    });
+    it('places multiple horizontal ships', () => {
+      const game = Gameboard();
+      const shipOne = Ship('small', 2);
+      const shipTwo = Ship('med', 3);
+      const shipThree = Ship('large', 4);
+      game.buildGrid();
+      game.placeShip(shipOne, 0, 8);
+      game.placeShip(shipTwo, 4, 0);
+      game.placeShip(shipThree, 8, 4);
+      expect(game.grid[0]).toEqual(['00', '01', '02', '03', '04', '05', '06', '07', 'small', 'small']);
+      expect(game.grid[4]).toEqual(['med', 'med', 'med', '43', '44', '45', '46', '47', '48', '49']);
+      expect(game.grid[8]).toEqual(['80', '81', '82', '83', 'large', 'large', 'large', 'large', '88', '89']);
     });
   });
 });
