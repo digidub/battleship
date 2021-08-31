59afc10b98de54f4df88bb8b3261a7d423307bdd
Sholto <madein.mania@gmail.com>
2021-08-31 13:40:07 +0100

    random move function for AI

--- src/components/player.js
+++ src/components/player.js
@@ -3,8 +3,22 @@ const player = () => {
     board.receiveAttack(x, y);
   };
 
+  const randomAttackCoordinates = () => Math.floor(Math.random() * 10);
+
+  const aiAttack = (board) => {
+    const x = randomAttackCoordinates();
+    const y = randomAttackCoordinates();
+    if (board.grid[x][y].hit === true) {
+      console.log(`hit already placed at ${x}${y}`);
+      aiAttack(board);
+    }
+    attack(board, x, y);
+    return { x, y };
+  };
+
   return {
     attack,
+    aiAttack,
   };
 };
 
