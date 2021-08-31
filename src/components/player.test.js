c9bf358e9f058a5eb56ffe6bf26f415915b2afb4
Sholto <madein.mania@gmail.com>
2021-08-31 11:12:02 +0100

    basic test for player making attack

--- src/components/player.test.js
+++ src/components/player.test.js
@@ -1,1 +1,20 @@
 const Player = require('./player');
+const Gameboard = require('./gameboard');
+
+describe('Player tests', () => {
+  describe('creates grid', () => {
+    it('creates a 10x10 grid', () => {
+      const playerOne = Player();
+      const playerTwo = Player();
+      const playerOneBoard = Gameboard();
+      playerOneBoard.buildGrid();
+      const playerTwoBoard = Gameboard();
+      playerTwoBoard.buildGrid();
+      playerOneBoard.createShips();
+      playerTwoBoard.createShips();
+      playerOneBoard.placeShip(playerOneBoard.ships[0], 0, 0);
+      playerTwo.attack(playerOneBoard, 0, 0);
+      expect(playerOneBoard.grid[0][0]).toStrictEqual({ hit: true, ship: { name: 'destroyer', startPos: '00' } });
+    });
+  });
+});
