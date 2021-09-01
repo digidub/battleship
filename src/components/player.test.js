9f29cbb324ff655e588244cac6c951a7ac13ae8d
Sholto <madein.mania@gmail.com>
2021-09-01 09:30:47 +0100

    cleaned up test

--- src/components/player.test.js
+++ src/components/player.test.js
@@ -4,7 +4,7 @@ const Gameboard = require('./gameboard');
 describe('Player tests', () => {
   describe('player interaction', () => {
     it('players can hit each others boards', () => {
-      const playerTwo = Player();
+      const playerTwo = Player(false);
       const playerOneBoard = Gameboard();
       playerOneBoard.buildGrid();
       const playerTwoBoard = Gameboard();
@@ -13,20 +13,19 @@ describe('Player tests', () => {
       playerTwoBoard.createShips();
       playerOneBoard.placeShip(playerOneBoard.ships[0], 0, 0);
       playerTwo.attack(playerOneBoard, 0, 0);
-      expect(playerOneBoard.grid[0][0]).toStrictEqual({ hit: true, ship: { name: 'destroyer', startPos: '00' } });
+      expect(playerOneBoard.grid[0][0]).toStrictEqual({
+        hit: true,
+        ship: { name: 'destroyer', startPos: '00' },
+      });
     });
   });
   describe('artificial intelligence', () => {
     it('places a random hit on the board', () => {
-      const playerTwo = Player();
+      const playerTwo = Player(true);
       const playerOneBoard = Gameboard();
       playerOneBoard.buildGrid();
-      const playerTwoBoard = Gameboard();
-      playerTwoBoard.buildGrid();
       playerOneBoard.createShips();
-      playerTwoBoard.createShips();
-      playerOneBoard.placeShip(playerOneBoard.ships[0], 0, 0);
-      const { x, y } = playerTwo.aiAttack(playerOneBoard, 0, 0);
+      const { x, y } = playerTwo.attack(playerOneBoard, 0, 0);
       expect(playerOneBoard.grid[x][y].hit).toStrictEqual(true);
     });
   });
