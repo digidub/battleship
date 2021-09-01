b210ce9fbd86194d21557815d5cae600d06c2a30
Sholto <madein.mania@gmail.com>
2021-09-01 09:30:40 +0100

    refactored to return hit status and coordinates

--- src/components/player.js
+++ src/components/player.js
@@ -2,31 +2,37 @@ const Player = (boolean) => {
   const isAI = boolean;
 
   const placeHit = (board, x, y) => {
-    const hit = board.receiveAttack(x, y);
-    if (hit === true) return true;
-    return false;
+    const checkHit = board.receiveAttack(x, y);
+    return { x, y, checkHit };
   };
 
-  const randomAttackCoordinates = () => Math.floor(Math.random() * 10);
-
-  const aiAttack = (board) => {
-    const x = randomAttackCoordinates();
-    const y = randomAttackCoordinates();
-    if (board.grid[x][y].hit === true) {
-      aiAttack(board);
-    }
-    placeHit(board, x, y);
+  const randomAttackCoordinates = () => {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
     return { x, y };
   };
 
+  const aiAttack = (board) => {
+    const { x, y } = randomAttackCoordinates();
+    if (board.grid[x][y].hit === true) {
+      aiAttack(board);
+    }
+    const hit = placeHit(board, x, y);
+    return hit;
+  };
+
   const attack = (board, x, y) => {
-    if (isAI === true) aiAttack(board);
-    else placeHit(board, x, y);
+    let hitCoordinates = {};
+    if (isAI === true) {
+      hitCoordinates = aiAttack(board);
+      return hitCoordinates;
+    }
+    hitCoordinates = placeHit(board, x, y);
+    return hitCoordinates;
   };
 
   return {
     attack,
-    aiAttack,
   };
 };
 
