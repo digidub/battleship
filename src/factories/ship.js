d596d0ec274a304cddfc7c84072cf3a7f180337d
Sholto <madein.mania@gmail.com>
2021-09-02 12:42:56 +0100

    remove old comments

--- src/factories/ship.js
+++ src/factories/ship.js
@@ -1,6 +1,3 @@
-// factory function to produce ships. Ships are objects
-// that include their length, where they have been hit, and whether or not they've been sunk
-
 const randomCoordinates = require('./randomCoordinates');
 
 const Ship = (name, length) => {
