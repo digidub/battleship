98bb1980110395c01af359fde4bb069cd0dd4958
Sholto <madein.mania@gmail.com>
2021-09-02 10:28:27 +0100

    test to ensure random number generator works

---
+++ src/factories/randomCoordinates.test.js
@@ -0,0 +1,13 @@
+const randomCoordinates = require('./randomCoordinates');
+
+describe('Random coordinates', () => {
+  describe('produces correct numbers', () => {
+    it('produces numbers between 0 and 9 inclusive', () => {
+      const { x, y } = randomCoordinates();
+      expect(x).toBeGreaterThanOrEqual(0);
+      expect(x).toBeLessThanOrEqual(9);
+      expect(y).toBeGreaterThanOrEqual(0);
+      expect(y).toBeLessThanOrEqual(9);
+    });
+  });
+});
