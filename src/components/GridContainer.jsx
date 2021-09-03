f7f50bd7934c33655e3b8526ac0e46e5ae7e0227
Sholto <madein.mania@gmail.com>
2021-09-03 09:33:49 +0100

    updates to display and styled components to start styling

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const GridContainer = (props) => {
   const playerGrid = props.grid.map((row) => {
-    return (
-      <div>
-        x
-        {row.map((column) => {
-          return <div>x</div>;
-        })}
-      </div>
-    );
+    return row.map((cell) => {
+      return <div>{cell.ship ? <p>{cell.ship.name}</p> : <p>blank</p>}</div>;
+    });
   });
 
-  return <div>{playerGrid}</div>;
+  return <GridBox>{playerGrid}</GridBox>;
 };
 
 export default GridContainer;
+
+const GridBox = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(10, 10%);
+`;
