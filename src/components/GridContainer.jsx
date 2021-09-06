4f9618d3e6ab15248ff2168335272cc04ecdf438
Sholto <madein.mania@gmail.com>
2021-09-06 09:38:24 +0100

    pass cell index as props to child

--- src/components/GridContainer.jsx
+++ src/components/GridContainer.jsx
@@ -6,7 +6,7 @@ const GridContainer = (props) => {
   const playerGrid = props.grid.map((row, indexX) => {
     return row.map((cell, indexY) => {
       return (
-        <Cell key={`${indexX}${indexY}`} clickFunction={props.clickFunction}>
+        <Cell key={`${indexX}${indexY}`} coordinates={`${indexX}${indexY}`} clickFunction={props.clickFunction}>
           {cell.ship ? <p>{cell.ship.name}</p> : <p>·</p>}
         </Cell>
       );
