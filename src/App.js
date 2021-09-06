75de90ba4cf1b611eca725de21fff4fcf1ee879a
Sholto <madein.mania@gmail.com>
2021-09-06 09:19:21 +0100

    import grid context and create provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import { useReducer } from 'react';
 import './App.css';
 import GridContainer from './components/GridContainer';
+import { GridContext } from './context';
 const gameController = require('./factories/gamecontroller');
 
-function reducer(state, action) {}
+function reducer(state, action) {
+  switch (action.type) {
+    case 'hit':
+      return {
+        ...state,
+      };
+    default:
+      throw new Error('woops');
+  }
+}
 
 function App() {
-  const [playerOne, playerOneUpdate] = useReducer(reducer, gameController.playerOne);
-  const [playerTwo, playerTwoUpdate] = useReducer(reducer, gameController.playerOne);
+  const [playerOne, playerOneUpdate] = useReducer(reducer, gameController.playerOne.board.grid);
+  // const [playerTwo, playerTwoUpdate] = useReducer(reducer, gameController.playerTwo.board.grid);
 
-  console.log(playerOne);
-  console.log(playerTwo);
+  const handleClick = (e) => {
+    console.log(e);
+  };
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <GridContainer grid={playerOne.board.grid}></GridContainer>
+        <h1>Battleships</h1>
       </header>
+      <GridContext.Provier value={{ playerOne, playerOneUpdate }}>
+        <GridContainer clickFunction={handleClick} grid={playerOne}></GridContainer>
+      </GridContext.Provier>
     </div>
   );
 }
