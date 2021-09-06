6e08a1b5bff5b621e054134e046442ba44eb6909
Sholto <madein.mania@gmail.com>
2021-09-06 09:37:56 +0100

    import context, begin dispatch function

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import styled from 'styled-components';
+import { GridContext } from '../context';
 
 const Cell = (props) => {
-  return <BattleCell onClick={props.clickFunction}>{props.children}</BattleCell>;
+  const { playerOneUpdate } = useContext(GridContext);
+
+  const handleClick = (e) => {
+    console.log(e);
+    playerOneUpdate({
+      type: 'hit',
+    });
+  };
+
+  return (
+    <BattleCell id={props.coordinates} onClick={props.clickFunction}>
+      {props.children}
+    </BattleCell>
+  );
 };
 
 export default Cell;
 
 const BattleCell = styled.div`
   border: 1px solid black;
+  &:hover {
+    cursor: crosshair;
+    background: #f0f0f0;
+    opacity: 0.5;
+  }
 `;
