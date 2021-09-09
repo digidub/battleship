07ec47219935b7249a8b4ddfbd2ee7f61addfa71
Sholto <madein.mania@gmail.com>
2021-09-09 08:20:57 +0100

    ui logic for changing turns

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ function App() {
   const [playerOneGridState, setPlayerOneGridState] = useState(gameController.playerOne.board.grid);
   const [playerTwoGridState, setPlayerTwoGridState] = useState(gameController.playerTwo.board.grid);
 
+  const hitLogic = (x, y) => {
+    const board = gameController.playerOne.attack(gameController.playerTwo.board, x, y);
+    if (board.checkHit) return true;
+    return false;
+  };
+
   const handleClick = (e) => {
+    console.log(e);
     const x = Number(e.target.id[0]);
     const y = Number(e.target.id[1]);
     if (playerTwoGridState[x][y].hit) return;
-    gameController.playerOne.attack(gameController.playerTwo.board, x, y);
+    let checkHit = hitLogic(x, y);
     setPlayerTwoGridState(gameController.playerTwo.board.grid);
+    if (checkHit) return;
+    else gameController.changeTurn();
+    setPlayerOneGridState(gameController.playerOne.board.grid);
   };
 
   return (
