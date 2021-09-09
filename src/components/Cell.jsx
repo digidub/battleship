ce3180dfb33d5a528ad94f8bd5e1fd672bea287f
Sholto <madein.mania@gmail.com>
2021-09-09 10:50:29 +0100

    attempted ship display hover logic, didn't work so commenting out for now

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Cell = (props) => {
-  return <BattleCell id={props.coordinates} onClick={props.clickFunction} ship={props.ship} hit={props.hit} />;
+  return <BattleCell id={props.coordinates} onClick={props.clickFunction} ship={props.ship} hit={props.hit} onMouseOver={props?.mouseOver} />;
 };
 
 export default Cell;
@@ -24,3 +24,20 @@ const BattleCell = styled.div`
     opacity: 0.5;
   }
 `;
+
+// const BattleCellHover = styled.div`
+//   border: 1px solid black;
+//   height: auto;
+//   background: grey;
+//   &:hover {
+//     cursor: crosshair;
+//     background: #f0f0f0;
+//     opacity: 0.5;
+//   }
+// `;
+
+// props.inGrid &&
+//     props.coordinates === `${props.coords[0]}` + `${props.coords[1]}` &&
+//     props.coordinates === `${props.coords[0]}` + `${props.coords[0] + props.ships[0].length}` ? (
+//     <BattleCellHover id={props.coordinates} onClick={props.clickFunction} ship={props.ship} hit={props.hit} onMouseOver={props?.mouseOver} />
+//   ) :
