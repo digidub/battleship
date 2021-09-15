84a7587c7fa340b485c9c40246a4130423901a73
Sholto <madein.mania@gmail.com>
2021-09-15 14:59:28 +0100

    conditional formatting for cell hover when placing ship

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -2,42 +2,32 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Cell = (props) => {
-  return <BattleCell id={props.coordinates} onClick={props.clickFunction} ship={props.ship} hit={props.hit} onMouseOver={props?.mouseOver} />;
+  return (
+    <BattleCell
+      id={props.coordinates}
+      onMouseEnter={props.handleHover}
+      onClick={props.handleClick}
+      isHovering={props.isHovering}
+      ship={props.ship}
+      hit={props.hit}
+    />
+  );
 };
 
 export default Cell;
 
-const handleCellColour = (props) => {
-  if (props.ship && props.hit) return 'red';
-  if (props.ship) return 'pink';
-  if (props.hit) return 'grey';
-  return 'white';
-};
+// const handleCellColour = (props) => {
+//   if (props.ship && props.hit) return 'red';
+//   if (props.ship) return 'pink';
+//   if (props.hit) return 'grey';
+//   return 'white';
+// };
 
 const BattleCell = styled.div`
   border: 1px solid black;
   height: auto;
-  background: ${(props) => handleCellColour(props)};
+  background: ${(props) => (props.isHovering && props.isHovering.includes(props.id) ? 'pink' : 'white')};
   &:hover {
     cursor: crosshair;
-    background: #f0f0f0;
-    opacity: 0.5;
   }
 `;
-
-// const BattleCellHover = styled.div`
-//   border: 1px solid black;
-//   height: auto;
-//   background: grey;
-//   &:hover {
-//     cursor: crosshair;
-//     background: #f0f0f0;
-//     opacity: 0.5;
-//   }
-// `;
-
-// props.inGrid &&
-//     props.coordinates === `${props.coords[0]}` + `${props.coords[1]}` &&
-//     props.coordinates === `${props.coords[0]}` + `${props.coords[0] + props.ships[0].length}` ? (
-//     <BattleCellHover id={props.coordinates} onClick={props.clickFunction} ship={props.ship} hit={props.hit} onMouseOver={props?.mouseOver} />
-//   ) :
