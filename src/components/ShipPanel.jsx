83e64fe399fca27c2de32ee9adb41f40805d250b
Sholto <madein.mania@gmail.com>
2021-09-15 14:45:00 +0100

    display of ships to place and diagram

--- src/components/ShipPanel.jsx
+++ src/components/ShipPanel.jsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const ShipPanel = () => {
-  return <div></div>;
+const ShipPanel = ({ ships, handleClick }) => {
+  const shipDiagram = (length) => {
+    return [...Array(length)].map((item) => {
+      return <ShipCell key={item} />;
+    });
+  };
+
+  const shipPanel = ships.map((ship) => {
+    return (
+      <div key={ship.name}>
+        <div>{ship.name}</div>
+        <ShipBox id={ship.name} onClick={handleClick}>
+          {shipDiagram(ship.length)}
+        </ShipBox>
+      </div>
+    );
+  });
+
+  return <div>{shipPanel}</div>;
 };
 
 export default ShipPanel;
+
+const ShipBox = styled.div`
+  width: 40px;
+  height: 40px;
+  display: grid;
+  grid-template-columns: repeat(5, 100%);
+  grid-auto-columns: min-content;
+`;
+
+const ShipCell = styled.div`
+  border: 1px solid black;
+  height: auto;
+  background: white;
+  &:hover {
+    cursor: pointer;
+  }
+`;
