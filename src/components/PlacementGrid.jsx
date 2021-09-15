b1077c3f7f39b6b063871ab1cb3a4d0a09b45f70
Sholto <madein.mania@gmail.com>
2021-09-15 14:59:05 +0100

    passing state props through individual grid cells. destructured props for readibility

--- src/components/PlacementGrid.jsx
+++ src/components/PlacementGrid.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import Cell from './Cell';
 import { GridBox } from './GridContainer';
 
-const PlacementGrid = (props) => {
-  const handleMouseOver = (e) => {
-    console.log(e);
-  };
-
-  const placementGrid = props.grid.map((row, indexX) => {
+const PlacementGrid = ({ grid, placingShip, isHovering, handleHover, handleClick }) => {
+  const placementGrid = grid.map((row, indexX) => {
     return row.map((cell, indexY) => {
-      return <Cell key={`${indexX}${indexY}`} coordinates={`${indexX}${indexY}`} mouseOver={handleMouseOver} />;
+      return (
+        <Cell
+          key={`${indexX}${indexY}`}
+          cellID={`${indexX}${indexY}`}
+          coordinates={`${indexX}${indexY}`}
+          isHovering={isHovering}
+          handleHover={handleHover}
+          handleClick={handleClick}
+        />
+      );
     });
   });
 
