1b505fadc0fd529c1e88d05666f116066d5e97a3
Sholto <madein.mania@gmail.com>
2021-09-20 16:09:25 +0100

    handle rotate - right click

--- src/components/PlacementGrid.jsx
+++ src/components/PlacementGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Cell from './Cell';
 import { GridBox } from './GridContainer';
 
-const PlacementGrid = ({ grid, placingShip, isHovering, handleHover, handleClick }) => {
+const PlacementGrid = ({ grid, placingShip, isHovering, handleHover, handleClick, handleRightClick }) => {
   const placementGrid = grid.map((row, indexX) => {
     return row.map((cell, indexY) => {
       return (
@@ -13,6 +13,8 @@ const PlacementGrid = ({ grid, placingShip, isHovering, handleHover, handleClick
           isHovering={isHovering}
           handleHover={handleHover}
           handleClick={handleClick}
+          handleRightClick={handleRightClick}
+          ship={cell.ship ? true : false}
         />
       );
     });
