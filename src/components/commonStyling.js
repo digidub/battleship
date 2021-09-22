654cedb9de80f21e8d047bf1d53a1242af6bfd50
Sholto <madein.mania@gmail.com>
2021-09-22 09:33:24 +0100

    module for common styles

---
+++ src/components/commonStyling.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  color: #494949;
+  text-transform: uppercase;
+  text-decoration: none;
+  background: #ffffff;
+  padding: 5px;
+  border: 4px solid #494949;
+  display: inline-block;
+  transition: all 0.4s ease 0s;
+  &:hover {
+    color: #ffffff;
+    background: #f6b93b;
+    border-color: #f6b93b;
+    transition: all 0.4s ease 0s;
+  }
+`;
